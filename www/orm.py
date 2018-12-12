3acdf40e570901b744267f9aff236a4b20fd0cef
AniLinga <anilinga@163.com>
2018-12-12 13:53:08 +0800

    orm update

--- www/orm.py
+++ www/orm.py
@@ -57,6 +57,159 @@ def execute(sql, args, autocommit=True):
             raise
         return affected
 
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ','.join(L)
+
+# 基类Field
+class Field(object):
+
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s,%s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+'''
+以下带Field的类都继承Filed，映射数据库的常见类型
+'''
+# 映射varchar
+class StringField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name,ddl,primary_key,default)
+
+# 映射bool类型
+class BooleanField(Field):
+
+    def __init__(self, name=None, default=False):
+        super.__init__(name, 'boolean', False, default)
+
+# 映射int类型
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super.__init__(name, 'bigint', primary_key, default)
+
+# 映射float类型
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super.__init__(name, 'real', primary_key, default)
+
+# 映射text类型
+class TextField(Field):
+
+    def __int__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
+
+class ModelMetaclass(type):
+
+    def __new__(cls, name, bases, attrs):
+        # 排除Model本身
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+        # 获取table名
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s(table:%s)' % (name, tableName))
+        # 获取所有的Field和主键名
+        mappings = dict[]
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+                if v.primary_key:
+                    # 找到主键
+                    if primaryKey: #判断主键是否有空值，有空值就抛出异常
+                        raise RuntimeError('Duplicate primary key for field:%s' % k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise RuntimeError('未找到主键')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings # 保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['primary_key'] = primaryKey # 主键属性名
+        attrs['__fields__'] = fields #除主键外的属性名
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s， `%s`) values(%s)' % (tableName, ', '.join(escaped_fields), primaryKey,create_args_string(len(escaped_fields)+1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f:'`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%S` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
+
+class Model(dict, ModelMetaclass):
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(" 'Model' has no attribute %s " % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueorDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.info('using default value for %s:%s' % (key, str(value)))
+                setattr(self, key, value)
+        return value
+
+    @classmethod
+    @asyncio.coroutine
+    def findAll(cls, where=None, args=None, **kw):
+        'find objects by where clause'
+        sql = [cls.__select__]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args=[]
+        orderby = kw.get('orderby', None)
+        if orderby:
+            sql.append('order by')
+            sql.append(orderby)
+        limit = kw.get('limit', None)
+        if limit:
+            sql.append('limit')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
