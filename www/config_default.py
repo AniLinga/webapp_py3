550c511dbb80f67d3ec8a516084671f8aefb4eba
AniLinga <anilinga@163.com>
2019-01-11 17:31:33 +0800

    configs update

---
+++ www/config_default.py
@@ -0,0 +1,16 @@
+'''Configuration'''
+
+__author__ = 'AlexZheng'
+
+configs = {
+    'db':{
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'root',
+        'password':'19950917',
+        'database':'awesome'
+    },
+    'session':{
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,11 @@
+'''
+Override Configurations
+'''
+
+__Author__ = 'Alex Zheng'
+
+configs = {
+    'db':{
+        'host':'127.0.0.1'
+    }
+}
